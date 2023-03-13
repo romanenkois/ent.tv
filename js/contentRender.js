8b2a236da570d41eb4f80edc52852ac38bd1ba3e
Illya <romanenkoisnot@gmail.com>
2023-03-13 19:42:40 +0200

    fix

--- js/contentRender.js
+++ js/contentRender.js
@@ -1,8 +1,6 @@
 const path = (location.href.substring(location.href.lastIndexOf('/') + 1)).split('.')[0];
 
-
-console.log(path);
-if (path === 'index' || path === '') {
+if (path === 'index') {
     fetch("/data.json")
     .then(response => response.json())
     .then(json => {
@@ -17,11 +15,43 @@ if (path === 'index' || path === '') {
                 `)
             } else {
                 document.querySelector('#main-content').insertAdjacentHTML("beforeend",`
-                        <div class="movie-render-frame">
+                        <div class="movie-render-frame col-6 col-lg-3">
                             <a href="watch.html?${json[i].title}"><img class="movie-img" src="${json[i].thumbnail.regular.small}" alt="хуйня"></a>
                             <p class="movie-info">${json[i].year} ${json[i].category} ${json[i].rating}</p>
                             <h3 class="movie-name">${json[i].title}</h3>
                         </div>
+                `)
+            }
+        }
+    })
+} else if (path === 'movies') {
+    fetch("/data.json")
+    .then(response => response.json())
+    .then(json => {
+        for (let i = 0; i < json.length; i++) {
+            if (json[i].category === "Movie") {
+                document.querySelector('#main-content').insertAdjacentHTML("beforeend",`
+                    <div class="movie-render-frame col-6 col-lg-3">
+                        <a href="watch.html?${json[i].title}"><img class="movie-img" src="${json[i].thumbnail.regular.small}" alt="хуйня"></a>
+                        <p class="movie-info">${json[i].year} ${json[i].category} ${json[i].rating}</p>
+                        <h3 class="movie-name">${json[i].title}</h3>
+                    </div>
+                `)
+            }
+        }
+    })
+} else if (path === 'tvseries') {
+    fetch("/data.json")
+    .then(response => response.json())
+    .then(json => {
+        for (let i = 0; i < json.length; i++) {
+            if (json[i].category === "TV Series") {
+                document.querySelector('#main-content').insertAdjacentHTML("beforeend",`
+                    <div class="movie-render-frame col-6 col-lg-3">
+                        <a href="watch.html?${json[i].title}"><img class="movie-img" src="${json[i].thumbnail.regular.small}" alt="хуйня"></a>
+                        <p class="movie-info">${json[i].year} ${json[i].category} ${json[i].rating}</p>
+                        <h3 class="movie-name">${json[i].title}</h3>
+                    </div>
                 `)
             }
         }
---
+++ js/search.js
@@ -0,0 +1,1 @@
+const searchField = document.querySelector('#search-field');
